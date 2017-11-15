839e71793c8d401fb0330d255810e0cf47328b08
Daniel Reszka <dannn.r@gmail.com>
2017-11-15 22:47:54 +0100

    server: setup basic socket.io

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
-import { createServer } from 'http';
+const cors = require('cors')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const port = process.env.PORT || 3000;
+const { PORT } = process.env || 3000;
 
-createServer(async (req, res) => {
-  await delay(500);
-  console.log('Request!');
-  res.end('hi!');
-})
-.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(cors())
+
+io.on('connection', socket =>
+  console.log('a user connected')
+);
+
+http.listen(PORT, () =>
+  console.log(`listening on *:${PORT}`)
+);
