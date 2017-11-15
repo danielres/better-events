94f48740c13d652f3269f7acd6077a533bd7b1c7
Daniel Reszka <dannn.r@gmail.com>
2017-11-15 23:16:21 +0100

    server: fix wrong assignment of PORT

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const { PORT } = process.env || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 
