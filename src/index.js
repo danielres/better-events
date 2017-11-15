54a4a9186ca549a4be76635b912dee9d5d7f91ff
Daniel Reszka <dannn.r@gmail.com>
2017-11-15 23:38:58 +0100

    render the received socket.io messages in the UI

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ const socket = io('http://localhost:3000')
 class App extends React.Component {
   state = {
     status: 'disconnected',
+    messages: [],
   }
 
   componentDidMount() {
     socket.on('connect', () => this.setState({ status: 'connected' }))
-    socket.on('event', data => console.log('event', data))
+    socket.on('message', message =>
+      this.setState({ messages: [...this.state.messages, message] })
+    )
     socket.on('disconnect', () => this.setState({ status: 'disconnected' }))
   }
 
@@ -20,6 +23,11 @@ class App extends React.Component {
       <div>
         <div>{this.state.status}</div>
         <h1>Hello world!</h1>
+        {this.state.messages.map(({ message, username }) => (
+          <li>
+            {username}: {message}
+          </li>
+        ))}
       </div>
     )
   }
