68505b7b1887e03edc56df2eb95b74a89f8ac4c9
Daniel Reszka <dannn.r@gmail.com>
2017-11-15 22:48:36 +0100

    add basic socket.io client (works with server)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
+import io from 'socket.io-client';
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>Hello world!</h1>, document.getElementById('root'));
+const socket = io('http://localhost:3000');
+
+socket.on('connect', function(){
+  console.log('connected');
+});
+
+socket.on('event', function(data){
+  console.log('event', data);
+});
+
+socket.on('disconnect', function(){
+  console.log('disconnected');
+});
+
+
+
+const App = () => (
+  <div>
+    <h1>Hello world!!</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
