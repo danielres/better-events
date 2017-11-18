840ce7147b0f2713c8db52f321cd83c951a63f63
Daniel Reszka <dannn.r@gmail.com>
2017-11-18 18:31:39 +0100

    enable flow type Message in server

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const app = require('express')()
 // flow-disable-next-line
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+import type { Message } from '../types'
 
 const PORT = process.env.PORT || 3000
 app.use(cors())
 
 let messages = []
 
-const persistMessage = message =>
+const persistMessage = (message: Message) =>
   new Promise((resolve, reject) => {
     messages = [...messages, message]
     resolve()
@@ -21,10 +22,13 @@ io.on('connection', socket => {
   console.log('a user connected')
   setTimeout(
     () =>
-      io.emit('message', {
-        username: 'socket.username',
-        message: `message ${i++}`,
-      }),
+      io.emit(
+        'message',
+        ({
+          authorId: 'authorId',
+          body: `message ${i++}`,
+        }: Message)
+      ),
     1000
   )
   socket.on('message', m => persistMessage(m).then(() => console.log('yeah')))
