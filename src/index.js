5171b7faa744cf952730cc88b2bd89da16bc7350
Daniel Reszka <dannn.r@gmail.com>
2017-11-18 18:32:29 +0100

    setup flow in frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+//@flow
 import io from 'socket.io-client'
-import React from 'react'
+import * as React from 'react'
 import { render } from 'react-dom'
+import type { Message } from '../types'
 
 const socket = io('http://localhost:3000')
 
-class App extends React.Component {
+type Props = {}
+type State = { status: 'connected' | 'disconnected', messages: Array<Message> }
+
+let i = 0
+
+class App extends React.Component<Props, State> {
   state = {
     status: 'disconnected',
     messages: [],
@@ -12,7 +19,7 @@ class App extends React.Component {
 
   componentDidMount() {
     socket.on('connect', () => this.setState({ status: 'connected' }))
-    socket.on('message', message =>
+    socket.on('message', (message: Message) =>
       this.setState({ messages: [...this.state.messages, message] })
     )
     socket.on('disconnect', () => this.setState({ status: 'disconnected' }))
@@ -23,9 +30,9 @@ class App extends React.Component {
       <div>
         <div>{this.state.status}</div>
         <h1>Hello world!</h1>
-        {this.state.messages.map(({ message, username }) => (
+        {this.state.messages.map(({ body, authorId }) => (
           <li>
-            {username}: {message}
+            {authorId}: {body}
           </li>
         ))}
       </div>
@@ -33,4 +40,6 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('root'))
+const rootEl = document.querySelector('root')
+if (!(rootEl instanceof Element)) throw new Error('invalid type')
+render(<App />, rootEl)
