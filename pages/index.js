647c74b091c382b9726d14a56938854f88eb7051
Daniel Reszka <dannn.r@gmail.com>
2017-11-19 18:08:21 +0100

    restore frontend

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,41 @@
-export default () => <div>Welcome to next.js!</div>
+import io from 'socket.io-client'
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const socket = io('http://localhost:3001')
+
+let i = 0
+
+export default class App extends React.Component {
+  state = {
+    status: 'disconnected',
+    messages: [],
+  }
+
+  componentDidMount() {
+    socket.on('connect', () => this.setState({ status: 'connected' }))
+
+    socket.on('message', message =>
+      this.setState({ messages: [...this.state.messages, message] })
+    )
+    socket.on('disconnect', () => this.setState({ status: 'disconnected' }))
+    socket.emit('message', {
+      authorId: 'authorId',
+      body: `message ${i++}`,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.status}</div>
+        <h1>Hello world!</h1>
+        {this.state.messages.map(({ body, authorId }) => (
+          <li>
+            {authorId}: {body}
+          </li>
+        ))}
+      </div>
+    )
+  }
+}
