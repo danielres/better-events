27981f5e3def215bdabd147c5d04955300787a1f
Daniel Reszka <dannn.r@gmail.com>
2017-11-26 19:30:06 +0100

    fix mistyped type naming (String -> string)

--- src/types.js
+++ src/types.js
@@ -5,4 +5,4 @@ export type Message = { authorId: string, body: string }
 export type Action =
   | { type: 'POST_MESSAGE_REQUEST' }
   | { type: 'POST_MESSAGE_SUCCESS', payload: Message }
-  | { type: 'POST_MESSAGE_FAILURE', error: String }
+  | { type: 'POST_MESSAGE_FAILURE', error: string }
