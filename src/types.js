a86d49098c55d822800301d940696356e28e2ed0
Daniel Reszka <dannn.r@gmail.com>
2017-11-26 18:21:12 +0100

    add POST_MESSAGE actions types

--- src/types.js
+++ src/types.js
@@ -1,2 +1,8 @@
 // @flow
+
 export type Message = { authorId: string, body: string }
+
+export type action =
+  | { type: 'POST_MESSAGE_REQUEST' }
+  | { type: 'POST_MESSAGE_SUCCESS', payload: Message }
+  | { type: 'POST_MESSAGE_FAILURE', error: String }
